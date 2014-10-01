e4b666997fbec9d3aeb756fc86a5a55c19a42c0c
Erik <info@zen4all.nl>
2014-10-01 10:34:47 +0200

    first files for version 1.3

--- dynamic_filter_1.5.1/uninstall.sql
+++
@@ -1,7 +0,0 @@
-SET @cid=0;
-SELECT (@cid:=configuration_group_id) as cid 
-FROM configuration_group
-WHERE configuration_group_title= 'Dynamic Filter';
-DELETE FROM configuration WHERE configuration_group_id = @cid;
-DELETE FROM configuration_group WHERE configuration_group_id = @cid;
-DELETE FROM admin_pages WHERE page_key='configDynamicFilter';
