a6a23dea3a82c2ea3bd3ffbb7b840a4cca35012b
Erik <info@zen4all.nl>
2014-10-01 10:38:21 +0200

    scripts for more/less butons updated

--- files for ZC 1.5.3/new files/includes/modules/pages/products_new/jscript_dynamic_filter.php
+++ files for ZC 1.5.3/new files/includes/modules/pages/products_new/jscript_dynamic_filter.php
@@ -8,7 +8,30 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: Damian Taylor
  */
-
 if (FILTER_NEW == 'Yes' && FILTER_STYLE != 'Dropdown' && FILTER_OPTIONS_STYLE == 'Expand') {
-echo '<script type="text/javascript" src="' . DIR_WS_TEMPLATE  . 'jscript/dynamic_filter/jquery.dynamic_filter.php"></script>';
+  ?>
+  <script type="text/javascript">
+    // Dynamic filter boxes
+    $("div.dynamicfilterContent").eq(0).ready(function () {
+  // Show more link if appropriate
+      $('ul.dFilterExpand').each(function () {
+        if ($(this).prop("scrollHeight") > 130)
+          $(this).height(130).siblings('a.dFilterToggle').show();
+      });
+  // Expand/collapse
+      $("a.dFilterToggle").click(function () {
+        if ($(this).siblings("ul.dFilterExpand").height() == 130) {
+          $('.dFilterToggleImg', $(this)).prop('src', $('.dFilterToggleImg').prop('src').replace('_more', '_less')).prop('alt', '<?php echo DYNAMIC_FILTER_TEXT_LESS; ?>').prop('title', '<?php echo DYNAMIC_FILTER_TEXT_LESS; ?>');
+          $(this).html($(this).html().replace("<?php echo DYNAMIC_FILTER_TEXT_MORE; ?>", "<?php echo DYNAMIC_FILTER_TEXT_LESS; ?>"));
+          $(this).siblings("ul.dFilterExpand").animate({height: $(this).siblings("ul.dFilterExpand").prop("scrollHeight")}, "slow");
+        } else {
+          $('.dFilterToggleImg', $(this)).prop('src', $('.dFilterToggleImg').prop('src').replace('_less', '_more')).prop('alt', '<?php echo DYNAMIC_FILTER_TEXT_MORE; ?>').prop('title', '<?php echo DYNAMIC_FILTER_TEXT_MORE; ?>');
+          $(this).html($(this).html().replace("<?php echo DYNAMIC_FILTER_TEXT_LESS; ?>", "<?php echo DYNAMIC_FILTER_TEXT_MORE; ?>"));
+          $(this).siblings("ul.dFilterExpand").animate({height: 130}, "slow");
+        }
+        return false;
+      });
+    });
+  </script>
+  <?php
 }
